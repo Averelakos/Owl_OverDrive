0f38d27afb1aa6a8f8e466afcd992d5cb2bd46c5
Iraklis Tsikas <tsikasiraklis@gmail.com>
2023-07-12 18:57:54 +0300

    backend: create service result and service result base

---
+++ Backend/Owl.Overdrive.Business/DTOs/ServiceResults/ServiceResult.cs
@@ -0,0 +1,9 @@
+﻿using Owl.Overdrive.Infrastructure.Services.Models;
+
+namespace Owl.Overdrive.Business.DTOs.ServiceResults
+{
+    public class ServiceResult<T> :ServiceResultBase
+    {
+        public T? Result { get; set; }
+    }
+}
---
+++ Backend/Owl.Overdrive.Infrastructure/Services/Models/ServiceResultBase.cs
@@ -0,0 +1,8 @@
+﻿namespace Owl.Overdrive.Infrastructure.Services.Models
+{
+    public class ServiceResultBase
+    {
+        public bool Success { get; set; } = true;
+        public string? Error { get; set; }
+    }
+}
